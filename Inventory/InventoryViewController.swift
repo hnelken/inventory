aaf79cc03e5dffb799c411b795eb9308229d2be6
Harry Nelken <harry.nelken@gmail.com>
2016-10-21 19:38:21 -0400

    abandoned header approach, UI not good enough
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

--- Inventory/InventoryViewController.swift
+++ Inventory/InventoryViewController.swift
@@ -68,21 +68,6 @@ class InventoryViewController: UIViewController, UITableViewDataSource, UITableV
         }
     }
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        // Return custom header view
-        return InventoryHeaderView(
-            inventory: self,
-            headerSection: section,
-            frame: CGRect(x: 0, y: 0,
-                          width: tableView.frame.size.width,
-                          height: CGFloat(kHeaderHeight))
-        )
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
-    }
-    
     // heightForRow
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 0 {
@@ -155,18 +140,7 @@ class InventoryViewController: UIViewController, UITableViewDataSource, UITableV
         }
     }
 
-    
-    // MARK: - Public API
-    func headerSelected(_ header: InventoryHeaderView) {
-     
-        // Change section header arrow image
-        header.arrow.image = (groupOpen[header.group])
-            ? UIImage(named: kClosedGroupImage)
-            : UIImage(named: kOpenGroupImage)
-        
-        toggleOpenStatus(for: header.group)
-    }
-    
+
     // MARK: - Private API
     
     // Refreshes the inventory data source
