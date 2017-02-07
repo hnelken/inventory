b7cc45278872a5e245ba9a7885cd8f91bbce8a2c
Harry Nelken <harry.nelken@gmail.com>
2017-02-07 14:10:21 -0500

    fixed range slider constaint issue
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

--- Inventory/SelectedItemViewController.swift
+++ Inventory/SelectedItemViewController.swift
@@ -144,10 +144,11 @@ class SelectedItemViewController: UIViewController, AKPickerViewDataSource, AKPi
     }
     
     override func viewDidLayoutSubviews() {
+        /*
         let margin: CGFloat = 20.0
         let width = view.bounds.width - 2.0 * margin
         rangeSlider?.frame = CGRect(x: margin, y: margin + topLayoutGuide.length,
-                                   width: width, height: 31.0)
+                                   width: width, height: 31.0) */
     }
     
     // MARK: - RangeSlider API
